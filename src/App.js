ff7050416436bb3d3d8803bef1b0711849f1e587
Mahmoud Gaber <mahmoudgaber94@gmail.com>
2022-03-31 13:25:52 +0200

    add HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from './components/Nav'
 import About from './components/About'
 import Shop from './components/Shop'
@@ -14,8 +14,8 @@ const App = () => {
       <main className="App min-vh-100">
         <Nav />
         <Routes>
-          <Route path="/" element={<About/>}  exact/>
-          <Route path="/about" element={<About/>}  />
+          <Route path="/" element={<About/>} exact/>
+          <Route path="/about" element={<About/>} />
           <Route path="/shop" element={<Shop/>} exact />
           <Route path="/shop/:id" element={<Product/>}/>
           <Route path="/todo" element={<Todo/>}/>
